4e1e5a0f4d8ff487b16c2b12de57fe81811525c0
Juliana da Silveira Aguilar <julianasilveiraaguilar@gmail.com>
2024-04-20 07:08:56 -0300

    Adding data to Companies and Departments

---
+++ INSERT_DATA.sql
@@ -0,0 +1,27 @@
+INSERT INTO Companies (name, cnpj, zip_code, neighborhood, city, state, phone, email)
+values('J&Tecnologia', '12.345.678/0001-99', '31280-500', 'Ouro Branco', 'Belo Horizonte', 'MG', '+55 31 6528-4693', 'contato@jtecnologia.com.br');
+
+INSERT INTO Departments(name)
+VALUES('Recursos Humanos'),
+      ('Vendas e Atendimento ao Cliente'),
+      ('Desenvolvimento de Software'),
+      ('Consultoria em TI'),
+      ('Suporte Técnico'),
+      ('Gestão de Projetos'),
+      ('Design de UX/UI'),
+      ('Engenharia de Dados'),
+      ('Qualidade de Software'),
+      ('Finanças'),
+      ('Operações'),
+      ('Pesquisa e Desenvolvimento'),
+      ('Logística'),
+      ('Compras'),
+      ('Planejamento Estratégico'),
+      ('Inovação'),
+      ('Treinamento e Desenvolvimento'),
+      ('Relações Públicas');
+
+
+
+
+
